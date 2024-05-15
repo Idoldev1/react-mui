ba58a4c4401addadd64bf61851292127a3e1e16e
Idoldev01 <idolayinka98@gmail.com>
2024-05-15 17:54:50 +0100

    Initial commit

---
+++ react-mui/src/components/MuiTypography.tsx
@@ -0,0 +1,19 @@
+import { Typography } from "@mui/material"
+
+export const MuiTypography = () => {
+    return (
+        <div>
+            <Typography variant="h1">h1 Heading</Typography>
+            <Typography variant="h2">h1 Heading</Typography>
+            <Typography variant="h3">h1 Heading</Typography>
+            <Typography variant="h4" component= 'h1' gutterBottom>h4 Heading</Typography>
+            <Typography variant="h5">h1 Heading</Typography>
+            <Typography variant="h6">h1 Heading</Typography>
+
+            <Typography variant="subtitle1">subtitle 1</Typography>
+            <Typography variant="subtitle2">subtitle 2</Typography>
+
+            
+        </div>
+    )
+}
